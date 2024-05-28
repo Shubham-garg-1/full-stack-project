efee67f1396eb0f740a05319ba6eac2f53f2b6e9
Shubham <shubh.gargofficial@gmail.com>
2024-05-28 21:28:31 +0530

    Changes made in app.js file

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use("/", userRouter);
  
 
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found!"));
+    next(new ExpressError(404, "Click on the Icon above or on Explore"));
 });
 
 app.use((err, req, res, next) => {
